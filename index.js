9a7c6fc0ae7b912f25427e4094e8349a04e8d68d
Merline Belizaire <merline@noukod.com>
2019-02-25 16:46:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function (drivers) {
+  return drivers.slice(0, 2);
+};
+
+returnFirstTwoDrivers(['Sally', 'Bob', 'Freddy', 'Claudia'])
+
+const returnLastTwoDrivers = function (drivers) {
+  return drivers.slice(-2);
+};
+
+returnLastTwoDrivers(['Sally', 'Bob', 'Freddy', 'Claudia'])
